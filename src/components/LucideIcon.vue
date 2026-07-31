d76703b755e798a59342b1f7678150ec432bf1af
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-07-31 04:37:58 +0200

    feat: add lucide-icon component

---
+++ src/components/LucideIcon.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { type LucideIcon } from '@lucide/vue'
+
+defineProps<{
+  icon: LucideIcon
+}>()
+</script>
+
+<template>
+  <component :is="icon" :size="24" />
+</template>
